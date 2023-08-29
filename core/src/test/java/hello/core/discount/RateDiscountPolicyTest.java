6cea4b3f9cdc415e357d48a1fa027c4a51c17a92
dhgus44 <dhgus44@naver.com>
2023-08-29 21:13:12 +0900

    공부내역
    .

---
+++ core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,38 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("10퍼센트 할인이 적용되어야 한다")
+    void vip_o() {
+        //given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+
+        //when
+        int disCount = discountPolicy.discount(member, 10000);
+
+        //then
+        assertThat(disCount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("Vip가 아니면 할인 적용x")
+    void vip_x() {
+        Member member = new Member(2L, "memberBasic", Grade.BASIC);
+
+        int disCount = discountPolicy.discount(member, 10000);
+
+        assertThat(disCount).isEqualTo(1000);
+    }
+
+}
